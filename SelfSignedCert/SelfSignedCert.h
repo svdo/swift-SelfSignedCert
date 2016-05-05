65fa3193c9acfcbd73507d3c1346b7472e92b54e
Stefan van den Oord <soord@mac.com>
2016-05-05 20:47:22 +0200

    Initial Commit

---
+++ SelfSignedCert/SelfSignedCert.h
@@ -0,0 +1,19 @@
+//
+//  SelfSignedCert.h
+//  SelfSignedCert
+//
+//  Created by Stefan on 05/05/16.
+//  Copyright © 2016 Stefan van den Oord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SelfSignedCert.
+FOUNDATION_EXPORT double SelfSignedCertVersionNumber;
+
+//! Project version string for SelfSignedCert.
+FOUNDATION_EXPORT const unsigned char SelfSignedCertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SelfSignedCert/PublicHeader.h>
+
+
