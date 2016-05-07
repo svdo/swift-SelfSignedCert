ee859646b93d4e6f9e8f9498109693802dfa7519
Stefan van den Oord <soord@mac.com>
2016-05-07 16:09:34 +0200

    Refactored production code to main target; cleaned up

--- SelfSignedCert/DEREncoding.swift
+++ SelfSignedCert/DEREncoding.swift
@@ -1,4 +1,87 @@
 //  Copyright © 2016 Stefan van den Oord. All rights reserved.
 
 import Foundation
+import SwiftBytes
 
+extension Bool {
+    func toDER() -> [UInt8] {
+        let value : UInt8 = self ? 0xFF : 0x00
+        return writeDER(tag:1, constructed:false, bytes:[value])
+    }
+}
+
+extension UnsignedIntegerType {
+    func toDER() -> [UInt8] {
+        var bytes : [UInt8] = SwiftBytes.bytes(self.toUIntMax()).removeLeadingZeros()
+        if (bytes[0] & 0x80) == 0x80 {
+            bytes = [0x00] + bytes
+        }
+        return writeDER(tag: 2, constructed: false, bytes: bytes)
+    }
+}
+
+extension IntegerType {
+    func toDER() -> [UInt8] {
+        if self >= 0 {
+            return UInt64(self.toIntMax()).toDER()
+        }
+        else {
+            let bitPattern = ~(-self.toIntMax()) + 1
+            let twosComplement = UInt64(bitPattern:bitPattern)
+            let bytes : [UInt8] = SwiftBytes.bytes(twosComplement).ensureSingleLeadingOneBit()
+            //[ UInt8(self.toIntMax() & 0x000000FF) ]
+            return writeDER(tag: 2, constructed: false, bytes: bytes)
+        }
+    }
+}
+
+
+
+extension SequenceType where Generator.Element:IntegerType {
+    func removeLeading(number:Generator.Element) -> [Generator.Element] {
+        if self.minElement() == nil { return [] }
+        var nonNumberFound = false
+        let flat = self.flatMap { (elem:Generator.Element) -> Generator.Element? in
+            if (elem == number && !nonNumberFound ) {
+                return nil
+            } else {
+                nonNumberFound = true
+                return elem
+            }
+        }
+        return flat
+    }
+    
+    func removeLeadingZeros() -> [Generator.Element] {
+        if self.minElement() == nil { return [] }
+        let removedZeros = self.removeLeading(0)
+        if removedZeros.count == 0 {
+            return [0]
+        }
+        else {
+            return removedZeros
+        }
+        
+    }
+    
+    func ensureSingleLeadingOneBit() -> [Generator.Element] {
+        if self.minElement() == nil { return [] }
+        let removedFF = self.removeLeading(0xFF)
+        if removedFF.count == 0 {
+            return [0xFF]
+        }
+        else if  removedFF[0] & 0x80 != 0x80 {
+            return [0xFF] + removedFF
+        }
+        return removedFF
+    }
+}
+
+func writeDER(tag tag:UInt8, constructed:Bool, bytes:[UInt8]) -> [UInt8] {
+    let constructedBits : UInt8 = (constructed ? 1 : 0) << 5
+    let classBits : UInt8 = 0
+    let headerByte1 : UInt8 = tag | constructedBits | classBits
+    let lengthBits : UInt8 = UInt8(bytes.count)
+    let headerByte2 : UInt8 = lengthBits
+    return [headerByte1, headerByte2] + bytes
+}
--- SelfSignedCertTests/DERTests.swift
+++ SelfSignedCertTests/DERTests.swift
@@ -10,21 +10,26 @@ class DERTests : QuickSpec {
     
     override func spec() {
         
-        fit("can encode basic types") {
-            expect(true.toDER()) == [0x01,0x01,0xFF]
-            expect(false.toDER()) == [0x01,0x01,0x00]
-            expect(0.toDER()) == [0x02,0x01,0x00]
-            expect(1.toDER()) == [0x02,0x01,0x01]
-            expect((-1).toDER()) == [0x02,0x01,0xFF]
-            expect(72.toDER()) == [0x02,0x01,0x48]
-            expect((-128).toDER()) == [0x02,0x01,0x80]
-            expect(128.toDER()) == [0x02,0x02,0x00,0x80]
-            expect(255.toDER()) == [0x02,0x02,0x00,0xFF]
-            expect((-256).toDER()) == [0x02,0x02,0xFF,0x00]
-            expect(12345.toDER()) == [0x02,0x02,0x30,0x39]
-            expect((-12345).toDER()) == [0x02,0x02,0xCF,0xC7]
-            expect(123456789.toDER()) == [0x02,0x04,0x07,0x5B,0xCD,0x15]
-            expect((-123456789).toDER()) == [0x02,0x04,0xF8,0xA4,0x32,0xEB]
+        describe("DER encoding") {
+            it("can encode booleans") {
+                expect(true.toDER()) == [0x01,0x01,0xFF]
+                expect(false.toDER()) == [0x01,0x01,0x00]
+            }
+            
+            it("can encode integers") {
+                expect(0.toDER()) == [0x02,0x01,0x00]
+                expect(1.toDER()) == [0x02,0x01,0x01]
+                expect((-1).toDER()) == [0x02,0x01,0xFF]
+                expect(72.toDER()) == [0x02,0x01,0x48]
+                expect((-128).toDER()) == [0x02,0x01,0x80]
+                expect(128.toDER()) == [0x02,0x02,0x00,0x80]
+                expect(255.toDER()) == [0x02,0x02,0x00,0xFF]
+                expect((-256).toDER()) == [0x02,0x02,0xFF,0x00]
+                expect(12345.toDER()) == [0x02,0x02,0x30,0x39]
+                expect((-12345).toDER()) == [0x02,0x02,0xCF,0xC7]
+                expect(123456789.toDER()) == [0x02,0x04,0x07,0x5B,0xCD,0x15]
+                expect((-123456789).toDER()) == [0x02,0x04,0xF8,0xA4,0x32,0xEB]
+            }
         }
         
         describe("Leading zeros") {
@@ -51,88 +56,4 @@ class DERTests : QuickSpec {
         }
     }
     
-}
-
-
-
-extension Bool {
-    func toDER() -> [UInt8] {
-        let value : UInt8 = self ? 0xFF : 0x00
-        return writeDER(tag:1, constructed:false, bytes:[value])
-    }
-}
-
-extension UnsignedIntegerType {
-    func toDER() -> [UInt8] {
-        var bytes : [UInt8] = SwiftBytes.bytes(self.toUIntMax()).removeLeadingZeros()
-        if (bytes[0] & 0x80) == 0x80 {
-            bytes = [0x00] + bytes
-        }
-        return writeDER(tag: 2, constructed: false, bytes: bytes)
-    }
-}
-
-extension IntegerType {
-    func toDER() -> [UInt8] {
-        if self >= 0 {
-            return UInt64(self.toIntMax()).toDER()
-        }
-        else {
-            let bitPattern = ~(-self.toIntMax()) + 1
-            let twosComplement = UInt64(bitPattern:bitPattern)
-            let bytes : [UInt8] = SwiftBytes.bytes(twosComplement).ensureSingleLeadingOneBit()
-            //[ UInt8(self.toIntMax() & 0x000000FF) ]
-            return writeDER(tag: 2, constructed: false, bytes: bytes)
-        }
-    }
-}
-
-
-
-extension SequenceType where Generator.Element:IntegerType {
-    func removeLeading(number:Generator.Element) -> [Generator.Element] {
-        if self.minElement() == nil { return [] }
-        var nonNumberFound = false
-        let flat = self.flatMap { (elem:Generator.Element) -> Generator.Element? in
-            if (elem == number && !nonNumberFound ) {
-                return nil
-            } else {
-                nonNumberFound = true
-                return elem
-            }
-        }
-        return flat
-    }
-    
-    func removeLeadingZeros() -> [Generator.Element] {
-        let removedZeros = self.removeLeading(0)
-        if removedZeros.count == 0 {
-            return [0]
-        }
-        else {
-            return removedZeros
-        }
-
-    }
-    
-    func ensureSingleLeadingOneBit() -> [Generator.Element] {
-        if self.minElement() == nil { return [] }
-        let removedFF = self.removeLeading(0xFF)
-        if removedFF.count == 0 {
-            return [0xFF]
-        }
-        else if  removedFF[0] & 0x80 != 0x80 {
-            return [0xFF] + removedFF
-        }
-        return removedFF
-    }
-}
-
-func writeDER(tag tag:UInt8, constructed:Bool, bytes:[UInt8]) -> [UInt8] {
-    let constructedBits : UInt8 = (constructed ? 1 : 0) << 5
-    let classBits : UInt8 = 0
-    let headerByte1 : UInt8 = tag | constructedBits | classBits
-    let lengthBits : UInt8 = UInt8(bytes.count)
-    let headerByte2 : UInt8 = lengthBits
-    return [headerByte1, headerByte2] + bytes
 }
