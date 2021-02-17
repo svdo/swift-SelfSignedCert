afd893a3095148f402369cebe4081677eb2ad564
Eirik Skogland <>
2021-02-17 14:28:34 +0100

    Remove cocoapods and use SPM instead.

--- Pods/Target Support Files/Pods-SelfSignedCert-SelfSignedCertTests/Pods-SelfSignedCert-SelfSignedCertTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SelfSignedCert_SelfSignedCertTests : NSObject
-@end
-@implementation PodsDummy_Pods_SelfSignedCert_SelfSignedCertTests
-@end
--- Pods/Target Support Files/SecurityExtensions/SecurityExtensions-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SecurityExtensions : NSObject
-@end
-@implementation PodsDummy_SecurityExtensions
-@end
