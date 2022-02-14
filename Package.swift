d79f49a39a65e171ed83939851c7b13466c4330c
Stefan van den Oord <soord@mac.com>
2022-02-14 20:30:54 +0100

    Bump min macos version because of dependency SwiftBytes

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SelfSignedCert",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
